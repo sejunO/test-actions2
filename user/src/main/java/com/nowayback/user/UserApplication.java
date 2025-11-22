fcfda3110406d575641efddd4ec7f5596505f339
sejunO <friendlyoh@gmail.com>
2025-11-22 21:47:58 +0900

    .

--- user/src/main/java/com/nowayback/user/UserApplication.java
+++ user/src/main/java/com/nowayback/user/UserApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserApplication {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
