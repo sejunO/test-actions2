b2716075aa26a1bb102daeec00a95f3112968b50
Cro <junsj1230@naver.com>
2025-11-24 00:15:33 +0900

    ci-cd test

--- user/src/main/java/com/nowayback/user/UserController.java
+++ user/src/main/java/com/nowayback/user/UserController.java
@@ -3,10 +3,12 @@ package com.nowayback.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
