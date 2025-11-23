241a31c7678a03e54614dbdfabb4391ec2fe23cd
Cro <junsj1230@naver.com>
2025-11-24 01:12:41 +0900

    ci-cd test

--- project/src/main/java/com/nowayback/project/ProjectApplication.java
+++ project/src/main/java/com/nowayback/project/ProjectApplication.java
@@ -1,11 +1,9 @@
 package com.nowayback.project;
 
-import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableS
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
