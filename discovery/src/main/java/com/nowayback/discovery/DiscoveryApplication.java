525b9dfeba8a91aee8038aec939c3ce782b4a3b0
Cro <junsj1230@naver.com>
2025-11-23 17:45:17 +0900

    ci-cd test

--- discovery/src/main/java/com/nowayback/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/nowayback/discovery/DiscoveryApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
-
     }
 
 }
